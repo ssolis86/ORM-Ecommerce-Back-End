1a79050d74d6c6a6ba6b3f4a209d893f0ca3af52
Stephen Solis <ssolis8619@gmail.com>
2021-04-24 00:37:55 -0500

    setup all, and findOne category route

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,18 +3,41 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      include: [{model: Product }]
+    });
+    res.json(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Catergory.findByPk(req.params.id, {
+      include: [{ model: Product, }]
+    });
+    
+    if (!categoryData) {
+      res.status(404).json({message: 'No category found with this id '});
+      return
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  
 });
 
 router.put('/:id', (req, res) => {
